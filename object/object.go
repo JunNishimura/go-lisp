f91542a3873617594671833fe0e92690bb672d2e
Jun Nishimura <n.junjun0303@gmail.com>
2024-07-01 01:28:35 +0900

    update object

--- object/object.go
+++ object/object.go
@@ -1,14 +1,24 @@
 package object
 
 import (
+	"bytes"
 	"fmt"
+
+	"github.com/JunNishimura/go-lisp/ast"
 )
 
 const (
-	ERROR_OBJ   = "ERROR"
-	NIL_OBJ     = "NIL"
-	INTEGER_OBJ = "INTEGER"
+	ERROR_OBJ    = "ERROR"
+	NIL_OBJ      = "NIL"
+	INTEGER_OBJ  = "INTEGER"
+	FUNCTION_OBJ = "FUNCTION"
+	SYMBOL_OBJ   = "SYMBOL"
+	CONSCELL_OBJ = "CONSCELL"
+	LIST_OBJ     = "LIST"
+	BUILTIN_OBJ  = "BUILTIN"
 )
+
+type BuiltInFunction func(args ...Object) Object
 
 type ObjectType string
 
@@ -35,3 +45,74 @@ type Integer struct {
 
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+
+type Symbol struct {
+	Name         string
+	Value        Object
+	PropertyList []Object
+	Function     *Function
+}
+
+func (s *Symbol) Type() ObjectType { return SYMBOL_OBJ }
+func (s *Symbol) Inspect() string  { return s.Value.Inspect() }
+
+type Function struct {
+	Parameters []*ast.Symbol
+	Body       ast.SExpression
+	Env        *Environment
+}
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	out.WriteString("(lambda (")
+	for i, p := range f.Parameters {
+		if i > 0 {
+			out.WriteString(" ")
+		}
+		out.WriteString(p.String())
+	}
+	out.WriteString(") ")
+	out.WriteString(f.Body.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
+type Builtin struct {
+	Fn BuiltInFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
+
+type ConsCell struct {
+	Car Object
+	Cdr Object
+}
+
+func (cc *ConsCell) Type() ObjectType { return CONSCELL_OBJ }
+func (cc *ConsCell) Inspect() string {
+	return fmt.Sprintf("(%s . %s)", cc.Car.Inspect(), cc.Cdr.Inspect())
+}
+
+type List struct {
+	SExpressions []Object
+}
+
+func (l *List) Type() ObjectType { return LIST_OBJ }
+func (l *List) Inspect() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	for i, s := range l.SExpressions {
+		if i > 0 {
+			out.WriteString(" ")
+		}
+		out.WriteString(s.Inspect())
+	}
+	out.WriteString(")")
+
+	return out.String()
+}
