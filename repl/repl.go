ad0c8be3fb277b77a2316bcd25e53d537f22f247
Jun Nishimura <n.junjun0303@gmail.com>
2024-07-01 01:29:23 +0900

    update repl

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/JunNishimura/go-lisp/evaluator"
 	"github.com/JunNishimura/go-lisp/lexer"
+	"github.com/JunNishimura/go-lisp/object"
 	"github.com/JunNishimura/go-lisp/parser"
 )
 
@@ -14,6 +15,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf("%s", PROMPT)
@@ -32,7 +34,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
