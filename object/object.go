f7331bdfddd777abde120f42a97133a378db6e29
Jun Nishimura <n.junjun0303@gmail.com>
2024-07-21 11:57:40 +0900

    delete special form object

--- object/object.go
+++ object/object.go
@@ -8,21 +8,19 @@ import (
 )
 
 const (
-	ERROR_OBJ       = "ERROR"
-	NIL_OBJ         = "NIL"
-	INTEGER_OBJ     = "INTEGER"
-	FUNCTION_OBJ    = "FUNCTION"
-	SYMBOL_OBJ      = "SYMBOL"
-	BUILTIN_OBJ     = "BUILTIN"
-	SPECIALFORM_OBJ = "SPECIALFORM"
-	QOUTE_OBJ       = "QUOTE"
-	MACRO_OBJ       = "MACRO"
-	CONSCELL_OBJ    = "CONSCELL"
-	LIST_OBJ        = "LIST"
+	ERROR_OBJ    = "ERROR"
+	NIL_OBJ      = "NIL"
+	INTEGER_OBJ  = "INTEGER"
+	FUNCTION_OBJ = "FUNCTION"
+	SYMBOL_OBJ   = "SYMBOL"
+	BUILTIN_OBJ  = "BUILTIN"
+	QOUTE_OBJ    = "QUOTE"
+	MACRO_OBJ    = "MACRO"
+	CONSCELL_OBJ = "CONSCELL"
+	LIST_OBJ     = "LIST"
 )
 
 type BuiltInFunction func(args ...Object) Object
-type SpecialFormFunction func(ast.SExpression, *Environment) Object
 
 type ObjectType string
 
@@ -90,13 +88,6 @@ type Builtin struct {
 
 func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
 func (b *Builtin) Inspect() string  { return "builtin function" }
-
-type SpecialForm struct {
-	Fn SpecialFormFunction
-}
-
-func (sf *SpecialForm) Type() ObjectType { return SPECIALFORM_OBJ }
-func (sf *SpecialForm) Inspect() string  { return "special form" }
 
 type Quote struct {
 	SExpression ast.SExpression
